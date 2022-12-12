2f7fd2c4f9b139b542a2db73eff91535cb6f6282
TimothyLetsoko <107189293+TimothyLetsoko@users.noreply.github.com>
2022-12-12 14:52:08 +0200

    Add files via upload

---
+++ mortgateCalculator/src/mortgateCalculator/MortgageCalculator.java
@@ -0,0 +1,50 @@
+package mortgateCalculator;
+
+import java.util.Scanner;
+
+public class MortgageCalculator {
+	
+	public static double calculator(double principal, double interestRate, int period) {
+		double mortgage=0;
+		//convert interest into monthly interest rate
+		interestRate/=12;
+		//then convert it into percentage
+		interestRate/=100;
+		
+		//convert period to months
+		period*=12;
+		
+		//formula for calculating mortgage
+		double numerator=interestRate*Math.pow(1+interestRate, period);
+		double denominator=Math.pow(1+interestRate, period)-1;
+		mortgage=principal*(numerator/denominator);
+		
+		return mortgage;
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		try {
+			System.out.print("Principal:");
+			double principal = input.nextDouble();
+			
+			System.out.print("Annual Interest Rate:");
+			double interestRate = input.nextDouble();
+
+			
+			System.out.print("Period(Years):");
+			int period = input.nextInt();
+
+			
+			//close input
+			input.close();
+			
+			//print mortgage
+			System.out.printf("Mortgage: $ %.2f", calculator(principal,interestRate,period));
+		}catch(Exception e) {
+			System.out.println("Something went wrong.");
+		}
+	}
+
+}
